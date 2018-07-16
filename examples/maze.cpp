7b274262e2fae75392b559535fafd6b8f54ce6f3
parsiad <parsiad.azimzadeh@gmail.com>
2018-07-16 11:43:53 -0400

    Cleaned up maze example

--- examples/maze.cpp
+++ examples/maze.cpp
@@ -1,4 +1,6 @@
+#include <iomanip>
 #include <iostream>
+
 #include <mfhowards>
 
 using namespace mfhowards;
@@ -11,21 +13,20 @@ int main() {
   ///////////////////
 
   constexpr double p_works = .8;  // Probability that robot works correctly
-  constexpr double discount = .9; // Discount factor
+  constexpr double discount = 1.; // Discount factor (1 means no discount)
 
   enum direction { north, east, south, west }; // Cardinal directions
   enum tile { E /* End */, O /* Walkable */, X /* Obstructed */ };
-
   constexpr int m = 3, n = 4;
   constexpr tile map[m][n] = {
-    {O, O, O, E},
-    {O, X, O, E},
-    {O, O, O, O},
+      {O, O, O, E},
+      {O, X, O, E},
+      {O, O, O, O},
   };
   constexpr double rewards[m][n] = {
-    {0., 0., 0., +1.},
-    {0., 0., 0., -1.},
-    {0., 0., 0.,  0.}
+      {-0.04, -0.04, -0.04, +1.00},
+      {-0.04, +0.00, -0.04, -1.00},
+      {-0.04, -0.04, -0.04, -0.04},
   };
 
   ///////////////////////
@@ -34,8 +35,8 @@ int main() {
 
   const auto P = [&](int i, int j, direction c) {
     // We have one state per node in the map along with an additional "sink"
-    // state, i == m * n. The robot is sent to the sink state when it reaches an
-    // end tile (denoted tile::E) signfying the end of the game.
+    // state, i == m * n. The robot is sent to the sink state when it reaches a
+    // tile of type tile::E, signifying the end of the game.
 
     // Convert the node i to its x and y coordinates
     const int xi = i % n;
@@ -50,9 +51,8 @@ int main() {
     // Probability that the robot malfunctions is p_fails * 2
     constexpr double p_fails = (1. - p_works) / 2.;
 
-    // p_north, p_east, p_south, p_west, and p_0 will hold the probability of
-    // the robot moving north, east, south, west, or staying in the same
-    // location.
+    // p_north, p_east, p_south, p_west, and p_0 are the probability of the
+    // robot moving north, east, south, west, or staying in the same location.
     double p_north = 0., p_east = 0., p_south = 0., p_west = 0., p_0 = 0.;
 
     if (yi == 0 || map[yi - 1][xi] == X) {
@@ -142,23 +142,48 @@ int main() {
   // Run Howard's algorithm //
   ////////////////////////////
 
-  auto results = howards_alg(bellman_eq_from_lambdas<direction>(
-    m * n + 1, // Number of states (+1 for sink state i == m * n)
-    {north, east, south, west}, // Controls are the cardinal directions
-    [&](int i, int j, direction c) {
-      // A = I - discount * P
-      const double kronecker_ij = (i == j ? 1. : 0.);
-      return kronecker_ij - discount * P(i, j, c);
-    },
-    [&](int i, direction) {
-      if (i == m * n) {
-        return 0.;
-      }
-      const int x = i % n, y = i / n;
-      return rewards[y][x];
-    }
-  ));
+  // The value of this Markov decision process is the unique solution v of
+  // min{ A(c)v - b(c) } == 0 where A(c) and b(c) are a real square matrix and
+  // real vector whose entries are given below.
 
-  cout << results;
+  const auto A = [&](int i, int j, direction c) {
+    // A = I - discount * P
+    const double kronecker_ij = (i == j ? 1. : 0.);
+    return kronecker_ij - discount * P(i, j, c);
+  };
+
+  const auto b = [&](int i, direction) {
+    if (i == m * n) {
+      return 0.;
+    }
+    const int x = i % n, y = i / n;
+    return rewards[y][x];
+  };
+
+  auto results = howards_alg(bellman_eq_from_lambdas<direction>(
+      m * n + 1, // Number of states (+1 for sink state i == m * n)
+      {north, east, south, west}, // Controls are the cardinal directions
+      A, b));
+
+  ///////////////////
+  // Print results //
+  ///////////////////
+
+  // Actual solution (from https://mpatacchiola.github.io/)
+  // +0.812 +0.868 +0.918 +1.000
+  // +0.762 +0.000 +0.660 -1.000
+  // +0.705 +0.655 +0.611 +0.388
+
+  {
+    int i = 0;
+    for (int y = 0; y < m; ++y) {
+      for (int x = 0; x < n; ++x) {
+        cout << std::fixed << std::setw(11) << std::setprecision(6)
+             << results.value(i++);
+      }
+      cout << endl;
+    }
+  }
+
   return results.status;
 }
